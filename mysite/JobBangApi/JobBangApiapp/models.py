7647505f700cff1f13fdec8c201be19de4076cc7
Marc-Antoine Bouchard <marcantoinebouchard2005@gmail.com>
2024-09-12 16:44:33 -0400

    model + routes

--- mysite/JobBangApi/JobBangApiapp/models.py
+++ mysite/JobBangApi/JobBangApiapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class users(models.Model):
+    email = models.CharField(max_length=255)
+    hash = models.IntegerField()
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    is_candidate = models.BooleanField()
+
+class candidate_info(models.Model):
+    user_id = models.ForeignKey(users, on_delete=models.CASCADE)
+    #more field later :)
+    
+class employer_info(models.Model):
+    company_name = models.IntegerField()
+    #more field later :)
+class offers(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    employer_id = models.ForeignKey(employer_info, on_delete=models.CASCADE)
+    
+def __str__(self):
+    return self.title
--- mysite/JobBangApi/JobBangApiapp/views.py
+++ mysite/JobBangApi/JobBangApiapp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    return Response("Hello, world!")
+
+#get user
+@api_view(['GET'])
+def read_users(request):
+    return Response("Hello, users!")
+
+#return individual user
+@api_view(['GET'])
+def read_user(request, id):
+    return Response(f"Hello, user {id}!")
+
+#create user
+@api_view(['POST'])
+def create_user(request):
+    return Response("Hello, new user!")
+
+#update user
+#@api_view(['PATCH'])
+#def update_user(request, id):
+#    return Response(f"Hello, user {id} updated!")
+
+#delete user
+#@api_view(['DELETE'])
+#def delete_user(request, id):
+#    return Response(f"Hello, user {id} deleted!")
+
+
