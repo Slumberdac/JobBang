56ee57bd14b14ccec6655a2f1bfa698b7b9dce61
Slumberdac <fougeresjacob@gmail.com>
2024-09-19 14:01:27 -0400

    Requirements

--- mysite/JobBangApi/JobBangApiapp/views.py
+++ mysite/JobBangApi/JobBangApiapp/views.py
@@ -3,33 +3,49 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def index(request):
     return Response("Hello, world!")
 
-#get user
-@api_view(['GET'])
+
+# get user
+@api_view(["GET"])
 def read_users(request):
     return Response("Hello, users!")
 
-#return individual user
-@api_view(['GET'])
+
+# return individual user
+@api_view(["GET"])
 def read_user(request, id):
     return Response(f"Hello, user {id}!")
 
-#create user
-@api_view(['POST'])
+
+# create user
+@api_view(["POST"])
 def create_user(request):
     return Response("Hello, new user!")
 
-#update user
-#@api_view(['PATCH'])
-#def update_user(request, id):
+
+# update user
+# @api_view(['PATCH'])
+# def update_user(request, id):
 #    return Response(f"Hello, user {id} updated!")
 
-#delete user
-#@api_view(['DELETE'])
-#def delete_user(request, id):
+# delete user
+# @api_view(['DELETE'])
+# def delete_user(request, id):
 #    return Response(f"Hello, user {id} deleted!")
 
+# Auth
 
+
+# login
+@api_view(["POST"])
+def login(request):
+    return Response("Hello, login!")
+
+
+# register
+@api_view(["POST"])
+def register(request):
+    return Response("Hello, register!")
